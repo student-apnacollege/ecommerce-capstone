628aaef1ee945bad47b7e365cfd43e460c777670
Aquib Ajani <aquibajani17@gmail.com>
2023-06-17 21:22:08 +0530

    add topnav

---
+++ frontend/src/components/others/TopNav.js
@@ -0,0 +1,16 @@
+import "./TopNav.css";
+import { BsTelephone } from "react-icons/bs";
+
+const TopNav = () => {
+  return (
+    <section id="topNav">
+      <div>
+        <BsTelephone /> + 51341313431434
+      </div>
+      <div>Shop all products at 50% off | Shop Now</div>
+      <div></div>
+    </section>
+  );
+};
+
+export default TopNav;
